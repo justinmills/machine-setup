55ac69d204ab2b4b2e76052d171ac08cda7862e8
Justin Mills <justin@diesellabs.com>
2019-03-22 14:44:36 -0400

    Use HOME instead of ~

--- README.md
+++ README.md
@@ -19,5 +19,14 @@ software you will need. These can be run and re-run as often as you like.
 
 Then you should run the PowerShell setup script (as administrator)
 
+Before doing so, however, make sure you have a HOME environment variable set and it points to your
+user's home directory:
+
+: echo $HOME
+
+To set it:
+
+: [Environment]::SetEnvironmentVariable("HOME", "C:\Users\justin", "justin")
+
 : iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/justinmills/machine-setup/master/windows/Setup.ps1'))
 
--- windows/Setup.ps1
+++ windows/Setup.ps1
@@ -43,19 +43,19 @@ Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 Write-Host ""
 Write-Host "Setting up emacs..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
-New-Item -ItemType Directory -Force -Path ~\code\personal
-git clone https://github.com/syl20bnr/spacemacs.git ~\code\personal\spacemacs
-git clone https://github.com/justinmills/emacs-spacemacs.git ~\code\personal\emacs-spacemacs
-New-Item -Path ~/.emacs.d -ItemType SymbolicLink -Value ~\code\personal\spacemacs
-New-Item -Path ~/.spacemacs.d -ItemType SymbolicLink -Value ~\code\personal\emacs-spacemacs\dot.spacemacs.d
+New-Item -ItemType Directory -Force -Path $HOME\code\personal
+git clone https://github.com/syl20bnr/spacemacs.git $HOME\code\personal\spacemacs
+git clone https://github.com/justinmills/emacs-spacemacs.git $HOME\code\personal\emacs-spacemacs
+New-Item -Path $HOME/.emacs.d -ItemType SymbolicLink -Value $HOME\code\personal\spacemacs
+New-Item -Path $HOME/.spacemacs.d -ItemType SymbolicLink -Value $HOME\code\personal\emacs-spacemacs\dot.spacemacs.d
 
 Set-PSReadLineOption -EditMode Emacs
 # -----------------------------------------------------------------------------
 Write-Host ""
 Write-Host "Setting up PowerShell Profile..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
-git clone https://github.com/justinmills/machine-setup.git ~\code\personal\machine-setup
-New-Item -Path $PROFILE -ItemType SymbolicLink -Value ~\code\personal\machine-setup\windows\PowerShell-profile.ps1
+git clone https://github.com/justinmills/machine-setup.git $HOME\code\personal\machine-setup
+New-Item -Path $PROFILE -ItemType SymbolicLink -Value $HOME\code\personal\machine-setup\windows\PowerShell-profile.ps1
 
 # Write-Host "------------------------------------" -ForegroundColor Green
 # Read-Host -Prompt "Setup is done, restart is needed, press [ENTER] to restart computer."
